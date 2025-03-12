43091624e7f7f4cfb1c99f96c60bc41e39c1002f
Lavrushkin Ivan <dragunov.8973@gmail.com>
2025-03-12 23:05:53 +0700

    First commit

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 DATA = {
     'omlet': {
@@ -19,12 +21,18 @@ DATA = {
     # можете добавить свои рецепты ;)
 }
 
-# Напишите ваш обработчик. Используйте DATA как источник данных
-# Результат - render(request, 'calculator/index.html', context)
-# В качестве контекста должен быть передан словарь с рецептом:
-# context = {
-#   'recipe': {
-#     'ингредиент1': количество1,
-#     'ингредиент2': количество2,
-#   }
-# }
+
+def recipe_view(request, recipe_name):
+    servings = int(request.GET.get('servings', 1))
+    # Получаем рецепт по имени
+    recipe = DATA.get(recipe_name)
+
+    if recipe is None:
+        return HttpResponse("Рецепт не найден", status=404)
+
+    # Умножаем количество ингредиентов на количество порций
+    context = {
+        'recipe': {ingredient: amount * servings for ingredient, amount in recipe.items()}
+    }
+
+    return render(request, 'calculator/index.html', context)
--- 1.2-requests-templates/recipes/recipes/urls.py
+++ 1.2-requests-templates/recipes/recipes/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 
 from django.urls import path
+from calculator.views import recipe_view
+
 
 urlpatterns = [
-    # здесь зарегистрируйте вашу view-функцию
+    path('<str:recipe_name>/', recipe_view, name='recipe_view'),
 ]
