7502392e2514710776ca1e267d1a5cef1b5ddff4
Lavrushkin Ivan <dragunov.8973@gmail.com>
2025-04-18 23:12:45 +0700

    makemigratios isnt working

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -2,6 +2,8 @@ import csv
 
 from django.core.management.base import BaseCommand
 from phones.models import Phone
+from django.utils.text import slugify
+from datetime import datetime
 
 
 class Command(BaseCommand):
@@ -12,6 +14,13 @@ class Command(BaseCommand):
         with open('phones.csv', 'r') as file:
             phones = list(csv.DictReader(file, delimiter=';'))
 
-        for phone in phones:
-            # TODO: Добавьте сохранение модели
-            pass
+            for phone in phones:
+                phone_object = Phone(id=int(phone[0]),
+                                     name=phone[1],
+                                     image=phone[2],
+                                     price=int(phone[3]),
+                                     release_date=datetime.strptime(phone[4],'%Y-%m-%d'),
+                                     lte_exists=bool(phone[5]),
+                                     slug=slugify(phone[1])
+                                     )
+                phone_object.save()
--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -2,5 +2,14 @@ from django.db import models
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30)
+    image = models.URLField()
+    price = models.IntegerField()
+    release_date = models.DateField()
+    lte_exists = models.BooleanField()
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return f'{self.id}|{self.name}|{self.price}|{self.lte_exists}|{self.slug}'
+
