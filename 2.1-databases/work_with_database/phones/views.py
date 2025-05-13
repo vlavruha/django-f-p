a186dcd9cab3f28d3f158f5307dff2da067a2d07
Lavrushkin Ivan <dragunov.8973@gmail.com>
2025-05-13 18:09:06 +0700

    makemigratios still isn`t working

--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,17 +1,36 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
+
+from phones.models import Phone
+
+SORTS = {
+    '-price': 'Greater price',
+    'price': 'Lower price',
+    '-release_date': 'Release date',
+}
 
 
-def index(request):
-    return redirect('catalog')
+def validate_sort(value):
+    if value == '' or value not in SORTS:
+        return next(iter(SORTS))
+    return value
 
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+
+    sort_current = validate_sort(request.GET.get('order_by'))
+    sort = {
+        'data': SORTS,
+        'current': sort_current,
+    }
+
+    phones = Phone.objects.all().order_by(sort_current)
+    context = {'phones': phones, 'sort': sort}
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    phone = Phone.objects.filter(slug=slug)[0]
+    context = {'phone': phone}
     return render(request, template, context)
